6285b312083ca14647dbf93ee097d3045148d0a0
meymeiziagaltiady <meymeizia@gmail.com>
2023-09-12 20:43:32 +0700

    upload web

---
+++ SpecialMission/server/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const UserModel = require("./models/Users");
+
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://localhost:27017/contoh");
+
+app.get("/getUsers", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching users" });
+  }
+});
+
+app.post("/createUser", async (req, res) => {
+  const user = req.body;
+  const newUser = new UserModel(user);
+  await newUser.save();
+
+  res.json(user);
+});
+
+app.listen(3001, () => {
+  console.log("SERVER SUCCESFULY RUN");
+});
